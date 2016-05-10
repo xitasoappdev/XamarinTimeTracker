2d7eeb9f3a1333456b8863c089b15ad20968d5f5
Liutauras Diu <liutauras.diu@xitaso.com>
2016-05-11 00:10:51 +0200

    Add converters

---
+++ XamarinTimeTracker/Converters/BoolToColorConverter0.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ProjectTracker
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+
+        public Color TrueColor { set; get; }
+
+        public Color FalseColor { set; get; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(value is bool))
+                return TrueColor;
+
+            return (bool)value ? TrueColor : FalseColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
