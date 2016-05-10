f6c8068029bc8a5aa38c3f579ff46ae59da00912
Liutauras Diu <liutauras.diu@xitaso.com>
2016-05-10 23:55:46 +0200

    Add Segment class

---
+++ XamarinTimeTracker/Model/Segment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinTimeTracker.Model
+{
+    class Segment
+    {
+        public DateTime StartTime { get; }
+
+        public DateTime? EndTime { get; internal set; }
+
+        public Segment(DateTime startTime)
+        {
+            StartTime = startTime;
+        }
+    }
+}
