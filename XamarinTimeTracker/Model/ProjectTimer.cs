61b8e85eb6fd720f1336e298001d0f6ee97fd6c6
Liutauras Diu <liutauras.diu@xitaso.com>
2016-05-11 14:14:44 +0200

    Add and implement ProjectTime class

---
+++ XamarinTimeTracker/Model/ProjectTimer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using XamarinTimeTracker.Model;
+
+namespace XamarinTimeTracker.Model
+{
+    public class ProjectTimer : ITimer
+    {
+
+        public event EventHandler<DateTime> Elapsed;
+
+        public void Start(TimeSpan interval)
+        {
+            Xamarin.Forms.Device.StartTimer(interval, () =>
+            {
+                Elapsed?.Invoke(this, Now);
+
+                return true;
+            });
+        }
+
+        public DateTime Now
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+        }
+
+    }
+}
+
