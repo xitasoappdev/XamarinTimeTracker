4e67cedf792b69dcfaa823a34f84250d570fd26f
Liutauras Diu <liutauras.diu@xitaso.com>
2016-05-11 15:18:20 +0200

    Attach ToggleProjectCommand to ItemTapped

--- XamarinTimeTracker/View/StartPage.xaml.cs
+++ XamarinTimeTracker/View/StartPage.xaml.cs
@@ -5,13 +5,20 @@ using Xamarin.Forms;
 
 namespace XamarinTimeTracker
 {
-	public partial class StartPage : ContentPage
-	{
-		public StartPage()
-		{
-			InitializeComponent();
-			BindingContext = App.Locator.Start;
-		}
-	}
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+            var viewModel = App.Locator.Start;
+            BindingContext = viewModel;
+
+            Projects.ItemTapped += (sender, e) =>
+            {
+                viewModel.ToggleProjectCommand.Execute(e.Item);
+                Projects.SelectedItem = null;
+            };
+        }
+    }
 }
 
