f6c9955c3ad4fc9413d70539ee0c43c2f7a3fafe
Liutauras Diu <liutauras.diu@xitaso.com>
2016-05-11 10:24:08 +0200

    Add Projects property to the StartViewModel

--- XamarinTimeTracker/ViewModel/StartViewModel.cs
+++ XamarinTimeTracker/ViewModel/StartViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
 
 namespace XamarinTimeTracker.ViewModel
@@ -23,6 +24,12 @@ namespace XamarinTimeTracker.ViewModel
         {
             get { return newProjectName; }
             set { Set(() => NewProjectName, ref newProjectName, value); }
+        }
+
+        public ObservableCollection<Project> Projects
+        {
+            get;
+            private set;
         }
     }
 }
